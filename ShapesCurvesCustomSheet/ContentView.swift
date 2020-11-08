17cacd834b3c01934da2ad9bd4908e1d6f3635a3
Max <maxim.macari.dam@gmail.com>
2020-11-08 19:11:58 +0100

    Initial Commit

---
+++ ShapesCurvesCustomSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShapesCurvesCustomSheet
+//
+//  Created by Maxim Macari on 08/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShapesCurvesCustomSheet/ShapesCurvesCustomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesCurvesCustomSheetApp.swift
+//  ShapesCurvesCustomSheet
+//
+//  Created by Maxim Macari on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ShapesCurvesCustomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
